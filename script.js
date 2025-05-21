3f97ad1fc4fb382efa635dd8cf0e96d7db5ca86c
joeishere-cyber <youssefmohamedpl71@gmail.com>
2025-05-21 19:15:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Lightbox Functionality
+function openLightbox(img) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxImg = document.getElementById('lightbox-img');
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    const lightbox = document.getElementById('lightbox');
+    lightbox.style.display = 'none';
+}
